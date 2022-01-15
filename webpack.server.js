c68c706755bca990ebbc8e4599cef9140bc83eae
Ahmed Said <ahmedelsaid@techtypes.com>
2022-01-15 11:55:19 +0200

    refactor :Ignoring Files with Webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const {merge} = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals=require('webpack-node-externals')
 const config = {
   // we should inform webpack that  we'are building a bundle
   // for Nodejs , rather than for the browser
@@ -17,5 +18,8 @@ const config = {
     // generate build file and append our bundle.js to this file "build"
     path: path.resolve(__dirname, "build"),
   },
+  //going to tell webpack to not bundle any libraries into our bundle on the server
+  // if that library exists inside the node module
+  externals:[webpackNodeExternals()]
 };
 module.exports = merge(baseConfig, config);
